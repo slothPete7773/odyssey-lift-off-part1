8bf62d799f8228c685f6d6de458b23310cb25da5
Veerakit SlothPete Prasertpol <veerakit413@gmail.com>
2024-06-20 11:07:58 +0700

    feat: gql schema

---
+++ server/src/schema.ts
@@ -0,0 +1,24 @@
+import gql from "graphql-tag"
+
+export const typeDefs = gql`
+    type Track {
+        id: ID!
+        title: String!
+        author: Author!
+        thumbnail: String
+        length: Int
+        modulesCount: Int
+    }
+
+    type Author {
+        id: ID!
+        name: String!
+        photo: String
+    }
+
+
+    type Query {
+        "Get tracks array for homepage grid"
+        tracksForHome: [Track!]!       
+    }
+`
